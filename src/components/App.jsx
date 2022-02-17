b0ea4ba4f13777ca9dc1cfa4ecdbe8fa97193cd0
Sergey Tarasov <tarasovsergeyrom@gmail.com>
2022-02-17 11:58:03 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 import API from './Api-service/apiService';
 
